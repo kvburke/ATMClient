6edb29acf91d764262fd627f3d03c17b3671d7b6
Kevin Burke <kvburke96@gmail.com>
2018-02-10 17:34:09 -0500

    changed class name.

--- src/main/java/com/burke/ATMController.java
+++ src/main/java/com/burke/ATMController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Scope(value="singleton")
 @Controller
-public class WelcomeController {
+public class ATMController {
 
 	
 	@Autowired
